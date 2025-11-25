3be9f45990cba4c77aa66650e21b8e9dc911a714
jalasalmi <juho.alasalmi@ptt.fi>
2025-11-25 17:55:22 +0200

    improve get_labels()

--- R/labels.R
+++ R/labels.R
@@ -6,13 +6,20 @@
 #' @export
 #'
 #' @examples
-get_labels <- function(table_code) {
+get_labels <- function(table_code, label = NULL) {
 
   url <- table_code_to_url(table_code)
   m <- pxweb::pxweb_get(url)
 
-  purrr::map(rlang::set_names(m$variables,
+  out <- purrr::map(rlang::set_names(m$variables,
                               sapply(m$variables, "[[", "code")),
-             ~rlang::set_names(.x$valueTexts, .x$values))
+             ~rlang::set_names(.x$valueTexts, .x$values)) |>
+    statfitools::clean_names()
+
+  if(!is.null(label)) {
+    label <- statfitools::make_names(label)
+    out <- out[[label]]
+  }
+  out
 
 }
